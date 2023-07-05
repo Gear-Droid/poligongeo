c2a3e120621d37fd03c094920bfc905a4690390e
Vlad <vladi003@yandex.ru>
2023-07-05 11:08:24 +0300

    DEBUG = False

--- poligongeo/poligongeo/settings.py
+++ poligongeo/poligongeo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%bccf66_%z@u82*)jwj3z+8a=uvca#xq=64h#w81v^4%gg+w42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['poligongeo-co.ru', 'www.poligongeo-co.ru']
 
