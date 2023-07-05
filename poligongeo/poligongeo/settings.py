5d6a8fa0f4f1e54e7a75b4186c0416733faa592a
Vlad <vladi003@yandex.ru>
2023-07-05 11:17:23 +0300

    settings fixes

--- poligongeo/poligongeo/settings.py
+++ poligongeo/poligongeo/settings.py
@@ -130,7 +130,10 @@ if DEBUG:
         BASE_DIR / '../static/',
         BASE_DIR / '../media/',
     )
-
+else:
+    STATICFILES_DIRS = (
+        BASE_DIR / '/static/',
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
