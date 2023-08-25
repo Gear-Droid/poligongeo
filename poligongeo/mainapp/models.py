6026ebcdfa8c32ae1a0233a446aee85780acf541
Vlad <vladi003@yandex.ru>
2023-08-26 01:01:50 +0300

    1

--- poligongeo/mainapp/models.py
+++ poligongeo/mainapp/models.py
@@ -14,3 +14,17 @@ class CallRequest(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+class CallRequest1(models.Model):
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+    first_name = models.CharField(max_length=128, verbose_name='Имя')
+    phone = models.CharField(max_length=128, verbose_name='Телефон')
+    email = models.CharField(max_length=128, verbose_name='Email')
+    created = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return str(self.id)
